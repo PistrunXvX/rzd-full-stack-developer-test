d1ebbf6ba20e4ea8d6fd3a129139e5b18146de50
EugenyX_X <workdjek@gmail.com>
2026-08-17 19:23:44 +0300

    feat: Добавил вывод данных в карточку поезда

--- rzd-front/src/App.vue
+++ rzd-front/src/App.vue
@@ -1,11 +1,44 @@
-<script setup lang="ts"></script>
+<script setup>
+  import { onMounted, ref } from 'vue';
+  import Card from './components/Card.vue';
+  import Data from "./assets/data.json";
+
+  const itemsCard = ref(Data.trains);
+
+  // onMounted(() => {
+  //     console.log(itemsCard.value);
+  // });
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-container>
+    <v-row>
+      <v-col 
+        v-for="(data, index) in itemsCard"
+        :key="index"
+        cols="12"
+        sm="6" 
+        md="4"
+      >
+        <Card :item="data"/>
+      </v-col>
+      <!-- <v-col cols="12" sm="6" md="4">
+        <Card />
+      </v-col>
+            <v-col cols="12" sm="6" md="4">
+        <Card />
+      </v-col>
+            <v-col cols="12" sm="6" md="4">
+        <Card />
+      </v-col>
+            <v-col cols="12" sm="6" md="4">
+        <Card />
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <Card />
+      </v-col> -->
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped></style>
---
+++ rzd-front/src/components/Card.vue
@@ -0,0 +1,92 @@
+
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+
+    const props = defineProps({
+        item: {
+            type: Object,
+            required: true,
+        }
+    });
+
+    const data = ref({
+        departure: "",
+        arrival: "",
+    });
+
+    const nearestDate = computed(() => getNearestDeparture(props.item.departures));
+
+    onMounted(() => {
+        data.value.departure = props.item.route[0];
+        data.value.arrival = props.item.route.at(-1);
+    })
+
+    function getNearestDeparture(depList) {
+        const dateNow = new Date();
+
+        dateNow.setHours(0, 0, 0, 0);
+
+        const upcomingDates = depList.map(d => new Date(d)).filter(d => d >= dateNow).sort((a, b) => a - b);
+        const nearestDate = upcomingDates[0];
+
+        return nearestDate.toLocaleDateString('ru-RU', {
+            day: "numeric",
+            month: "short",
+            year: "numeric"
+        });
+    }
+</script>
+
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">
+          {{ props.item.name }}
+        </v-list-item-title>
+        <v-list-item-subtitle>{{ props.item.region }}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-text>
+      <v-row align="center">
+        <v-col cols="12">
+          <v-icon left>mdi-map-marker</v-icon>
+          {{ data.departure }} &rarr; {{ data.arrival }}
+        </v-col>
+        <v-col cols="12">
+          <v-icon left>mdi-clock</v-icon>
+          {{ props.item.duration_days }} дней
+        </v-col>
+        <v-col cols="12">
+          <v-icon left>mdi-calendar</v-icon>
+            {{ nearestDate }}
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        text
+        color="primary"
+        variant="flat"
+      >
+        Подробнее
+      </v-btn>
+      <v-spacer></v-spacer>
+      <div class="subtitle-1">
+        от {{ props.item.price_from }} ₽
+      </div>
+    </v-card-actions>
+  </v-card>
+</template>
